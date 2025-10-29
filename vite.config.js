e6badc516dd04b96055fc164ae0f94785ef70109
Silvineey <lucreciasilvio@hotmail.com>
2025-10-29 12:03:31 -0300

    teste
    teste

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Login-React",
+  base: "/Login-React/",
   plugins: [
     react({
       babel: {
